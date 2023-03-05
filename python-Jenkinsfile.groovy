ce3120fa9c3278f34d1b4368fe233193740149b5
Ahsan <muhammad.ahsan@gmail.com>
2023-03-05 17:43:14 +0100

    Update python-Jenkinsfile.groovy

--- python-Jenkinsfile.groovy
+++ python-Jenkinsfile.groovy
@@ -22,7 +22,7 @@ pipeline {
                 script {
                     sh 'python3 --version'
                     sh 'python3 -m pip install virtualenv'
-                    sh 'python3 -m virtualenv -p ˙which pyhton3.9˙ .venv'
+                    sh 'python3 -m virtualenv -p "which pyhton3.9" .venv'
                     sh 'source .venv/bin/activate'
                     sh 'whereis python3'
                     sh 'python3 -m pip install --upgrade pip'
