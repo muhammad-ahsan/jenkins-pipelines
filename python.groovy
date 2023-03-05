521f0f5fd71445ecc606e094a3f0b6b369dd46a2
Ahsan <muhammad.ahsan@gmail.com>
2023-03-05 12:36:29 +0100

    Update python.groovy

--- python.groovy
+++ python.groovy
@@ -32,8 +32,8 @@ pipeline {
         stage('Code Linting'){
            steps {
                   
-                sh 'python3 -m pylint --output-format=parseable --fail-under=<threshold value> module --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.log || echo "pylint exited with $?"'
-                echo "Linting Success, Generating Report"
+                sh 'python3 -m pylint --output-format=parseable --fail-under=1 src --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.log || echo "pylint exited with $?"'
+                echo "linting Success, Generating Report"
                 recordIssues enabledForFailure: true, aggregatingResults: true, tool: pyLint(pattern: 'pylint.log')
 
                
