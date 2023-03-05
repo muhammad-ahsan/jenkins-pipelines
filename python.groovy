102a4258fc5b4c8644f3488ec6dd0501c9ee0123
Ahsan <muhammad.ahsan@gmail.com>
2023-03-05 14:27:56 +0100

    Update python.groovy

--- python.groovy
+++ python.groovy
@@ -32,12 +32,23 @@ pipeline {
         stage('Code Linting'){
            steps {
                 sh 'python3 -m pylint --output-format=parseable --fail-under=0 $(git ls-files "*.py") --msg-template="{path}:{line}: [{msg_id}({symbol}), {obj}] {msg}" | tee pylint.log || echo "pylint exited with $?"'
-                echo "linting Success, Generating Report"
-                // recordIssues enabledForFailure: true, aggregatingResults: true, tool: pyLint(pattern: 'pylint.log')
-                recordIssues healthy: 1, minimumSeverity: 'NORMAL', unhealthy: 9, qualityGates: [[threshold: 1, type: 'TOTAL_NORMAL', unstable: true], [threshold: 1, type: 'TOTAL_HIGH', unstable: false], [threshold: 1, type: 'TOTAL_ERROR', unstable: false]], tools: [pyLint(pattern: 'pylint.log')]
+                tool: pyLint(pattern: 'pylint.log')
+                echo "Generating Report - Linting Success"
+                
+                stash name: 'pylint.log'
             }
         }
-        
+        stage('Analyze Linting Results') {
+            steps {
+                unstash 'pylint.log'
+                recordIssues healthy: 1, minimumSeverity: 'NORMAL', unhealthy: 9, qualityGates: [[threshold: 1, type: 'TOTAL_NORMAL', unstable: true], [threshold: 1, type: 'TOTAL_HIGH', unstable: false], [threshold: 1, type: 'TOTAL_ERROR', unstable: false]], tools: [pyLint(pattern: 'pylint.log')]
+            }
+            post {
+                failure {
+                    error('Abort because of pylint warnings')
+                }
+            }
+        }
         
         stage('Code Testing'){
            steps {
