061f6568defe05f01fb668eb2b0c2c6330ab7c29
Muhammad Ahsan <muhammad.ahsan@ginkgo-analytics.com>
2023-03-05 20:32:44 +0100

    Minor update

--- python-Jenkinsfile.groovy
+++ python-Jenkinsfile.groovy
@@ -8,7 +8,7 @@ pipeline {
                 // Check OS details of Jenkins Server
                 sh 'uname -mrs'
                 // Check docker integration with Jenkins
-                docker --version
+                sh '/usr/local/bin/docker --version'
             }
         }
         stage("Checkout Code") {
